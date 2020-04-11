2e8c71392947964d44375699a3a1e715b3a1855c
jovan-123 <61934098+jovan-123@users.noreply.github.com>
2020-04-11 12:45:30 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Jovan", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Nikola", 30, "191128", "Math");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Mohamad", 19, "2828", "mohamad@amikom.ac.id");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
